485e8b93788fadde69b389609b6c7c4fd29212db
ymgig <37842585+ymgig@users.noreply.github.com>
2018-05-01 22:17:02 -0400

    Update search.js

--- src/js/search.js
+++ src/js/search.js
@@ -1,10 +1,11 @@
 function runSearch( term ) {
-    $('#results').hide();
-    $('tbody').empty();
-    var frmStr = $('#cpg_dat').serialize();
+    $("#results").hide();
+    $("tbody").empty();
+    //var frmStr = {dna_seq:$("#dna_seq").val(),frame_size:$("#frame_size").val(),oe_ratio:$("#oe_ratio").val()};
+    var frmStr = $("form").serialize()
     $.ajax({
-        url: './search.cgi',
-        dataType: 'json',
+        url: "./search.cgi",
+        dataType: "json",
         data: frmStr,
         success: function(data, textStatus, jqXHR) {
             processJSON(data);
@@ -17,21 +18,22 @@ function runSearch( term ) {
 }
 
 function processJSON( data ) {
-    $('#match_count').text( data.match_count );
+    $("#match_count").text( data.match_count );
     var next_row_num = 1;
     $.each( data.matches, function(i, item) {
-        var this_row_id = 'result_row_' + next_row_num++;
-        $('<tr/>', { "id" : this_row_id } ).appendTo('tbody');
-        $('<td/>', { "text" : item.start } ).appendTo('#' + this_row_id);
-        $('<td/>', { "text" : item.end } ).appendTo('#' + this_row_id);
-                $('<td/>', { "text" : item.count } ).appendTo('#' + this_row_id);
-                $('<td/>', { "text" : item.percent } ).appendTo('#' + this_row_id);
-                $('<td/>', { "text" : item.ratio } ).appendTo('#' + this_row_id);
-        }
+        var this_row_id = "result_row_" + next_row_num++;
+        $("<tr/>", { "id" : this_row_id } ).appendTo("tbody");
+        $("<td/>", { "text" : item.start } ).appendTo("#" + this_row_id);
+        $("<td/>", { "text" : item.end } ).appendTo("#" + this_row_id);
+        $("<td/>", { "text" : item.count } ).appendTo("#" + this_row_id);
+        $("<td/>", { "text" : item.percent } ).appendTo("#" + this_row_id);
+        $("<td/>", { "text" : item.ratio } ).appendTo("#" + this_row_id);
+    })
+    $('#results').show();
 }
-	   
+
 $(document).ready( function() {
-    $('#submit').click( function() {
+    $("#submit").click( function() {
         runSearch();
         return false;
     });
