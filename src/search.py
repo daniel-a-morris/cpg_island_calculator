9169ebe97cab6dd98761d237b1e697170214fabb
ymgig <37842585+ymgig@users.noreply.github.com>
2018-05-01 22:22:59 -0400

    Update search.py

--- src/search.py
+++ src/search.py
@@ -2,23 +2,21 @@
 
 import cgi, json
 import os
-#import mysql.connector
 import cgitb
 cgitb.enable(display=0, logdir="log/")
 
 def main():
     print("Content-Type: application/json\n\n")
     form = cgi.FieldStorage()
-    print(form)
     sequence = form.getvalue('dna_seq')
-    frame = form.getlist('frame_size')
-    threshold = form.getlist('oe_ratio')
+    frame = form.getvalue('frame_size')
+    frame = int(frame)
+    threshold = form.getvalue('oe_ratio')
+    threshold = float(threshold)
     results = { 'match_count': 0, 'matches': list() }
     c = 0
     g = 0
     cpg = 0
-
-    #algorithm
     for i in range(0,len(sequence)-frame):
         temp = sequence[i:i+frame]
         for j in range(0,frame):
@@ -33,9 +31,9 @@ def main():
                 continue
         per = (c+g)/(frame/100)
         ratio = (cpg*frame)/(c*g)
-        if(ratio >=threshold and per >= 50):
-            results['matches'].append({'start': (i+1),'end': (i+frame+1),'count':cpg,'percent':per,'ratio':ratio})
+        if(ratio >= threshold and per >= 50):
             results['match_count'] += 1
+            results['matches'].append({'start': (i+1),'end': (i+frame),'count':cpg,'percent':per,'ratio':ratio})
         c = 0
         g = 0
         cpg = 0
